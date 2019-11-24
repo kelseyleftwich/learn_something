80820750b79604163c1453edf33dfc292f6eb6b9
Kelsey Leftwich <kelsey.leftwich@gmail.com>
2019-11-24 11:20:40 -0600

    Removes test

--- test/learn_something_web/controllers/page_controller_test.exs
+++ test/learn_something_web/controllers/page_controller_test.exs
@@ -1,8 +1,5 @@
 defmodule LearnSomethingWeb.PageControllerTest do
   use LearnSomethingWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Learn Something"
-  end
+
 end
